9b6cbe0e7a0567ea69a85195b4a34f6b366c4ada
Yves Lorenzo <veslorens@gmail.com>
2017-05-23 00:22:13 +0800

    Start my portfolio.

--- index.php
+++ index.php
@@ -1,3 +1,329 @@
-<?php
 
-die('Yves Lorenzo Yee');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="shortcut icon" href="style/images/favicon.png">
+<title>Yves Lorenzo</title>
+<!-- Bootstrap core CSS -->
+<link href="style/css/bootstrap.min.css" rel="stylesheet">
+<link href="style/css/plugins.css" rel="stylesheet">
+<link href="style/css/prettify.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<link href="style/css/color/green.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,800,700,600,500,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic' rel='stylesheet' type='text/css'>
+<link href="style/type/fontello.css" rel="stylesheet">
+<link href="style/type/budicons.css" rel="stylesheet">
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+      <script src="style/js/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+      <![endif]-->
+</head>
+<body class="full-layout">
+<div id="preloader"><div id="status"><div class="spinner"></div></div></div>
+<div class="body-wrapper">
+  <nav class="navbar navbar-default" role="navigation">
+    <div class="navbar-header"> <a class="btn responsive-menu" data-toggle="collapse" data-target=".navbar-collapse"><i></i></a>
+      <div class="navbar-brand text-center"> <a href="/"><img src="style/images/logo.png" alt="" data-src="style/images/logo.png" data-ret="style/images/logo@2x.png" class="retina" /></a> </div>
+      <!-- /.navbar-brand --> 
+    </div>
+    <!-- /.navbar-header -->
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="current"><a href="#home" class="hint--right" data-hint="Home"><i class="budicon-home-1"></i><span>Home</span></a></li>
+        <li><a href="#about" class="hint--right" data-hint="About"><i class="budicon-author"></i><span>About</span></a></li>
+        <li><a href="#contact" class="hint--right" data-hint="Contact"><i class="budicon-profile"></i><span>Contact</span></a></li>
+      </ul>
+      <!-- /.navbar-nav --> 
+    </div>
+    <!-- /.navbar-collapse -->
+    <div id="elsewhere" style="display:none;">
+      <h1>Me, Elsewhere</h1>
+      <div class="divide20"></div>
+      <ul class="social">
+        <li><a href="#"><i class="icon-s-twitter"></i></a></li>
+        <li><a href="#"><i class="icon-s-facebook"></i></a></li>
+        <li><a href="#"><i class="icon-s-instagram"></i></a></li>
+        <li><a href="#"><i class="icon-s-flickr"></i></a></li>
+        <li><a href="#"><i class="icon-s-pinterest"></i></a></li>
+        <li><a href="#"><i class="icon-s-linkedin"></i></a></li>
+      </ul>
+    </div>
+    <!-- /#elsewhere --> 
+  </nav>
+  <!-- /.navbar -->
+  
+  <section id="home" class="naked">
+    <div class="fullscreenbanner-container revolution">
+      <div class="fullscreenbanner">
+        <ul>
+          <li data-transition="fade"> <img src="style/images/dummy.png"  alt="slidebg1" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="repeat">
+            <h1 class="tp-caption caption large sfb" data-x="center" data-y="center" data-voffset="-25" data-speed="900" data-start="1000" data-endspeed="100" data-easing="Sine.easeOut">Yves Lorenzo Yee</h1>
+            <div class="tp-caption small tp-fade fadeout tp-resizeme" data-x="center" data-y="center" data-voffset="25" data-speed="100"
+			data-start="1500"
+			data-easing="Power4.easeOut"
+			data-splitin="chars"
+			data-splitout="chars"
+			data-elementdelay="0.03"
+			data-endelementdelay="0"
+			data-endspeed="100"
+			data-endeasing="Power1.easeOut"
+			style="z-index: 3; max-width: auto; max-height: auto; white-space: nowrap;">
+						Full stack spider at web. "Jack of most trades, Master of some."
+					</div>
+            <div class="arrow smooth"><a href="#about"><i class="icon-down-open-big"></i></a></div>
+          </li>
+        </ul>
+        <div class="tp-bannertimer"></div>
+      </div>
+      <!-- /.fullscreenbanner --> 
+    </div>
+    <!-- /.revolution --> 
+  </section>
+  <!-- /#home -->
+  
+  <div class="container">
+    
+    <section id="about">
+      <div class="box">
+        <h2 class="section-title">The Spider</h2>
+        <div class="row">
+	      <div class="col-md-5 col-md-push-7 col-sm-12">
+            <figure class="frame"><img src="style/images/art/about.jpg" alt="" /></figure>
+          </div>
+          <!-- /column -->
+          <div class="col-md-7 col-md-pull-5 col-sm-12">
+            <p class="lead">Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna. </p>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus. </p>
+            <p>Tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+            <p>Fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla.</p>
+            <p>Tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+        <div class="clearfix"></div>
+        <div class="divide40"></div>
+        <h2 class="section-title">My Skills</h2>
+        <div class="divide10"></div>
+        <div class="services-1">
+          <div class="row">
+            <div class="col-md-4 col-sm-6">
+              <div class="icon"> <i class="budicon-diamond icn"></i> </div>
+              <!-- /.icon -->
+              <div class="text">
+                <h5>Yfful</h5>
+                <p>Nulla vitae libero, a pharetra augue. Integer posuere a ante venenatis condimentum. Donec id elit non mi.</p>
+              </div>
+              <!-- /.text --> 
+            </div>
+            <!-- /column -->
+            <div class="col-md-4 col-sm-6">
+              <div class="icon"> <i class="budicon-diamond icn"></i> </div>
+              <!-- /.icon -->
+              <div class="text">
+                <h5>Oroz</h5>
+                <p>Vestibulum id ligula porta euismod semper. Aenean lacinia bibendum vestibulum. Cum sociis natoque.</p>
+              </div>
+              <!-- /.text --> 
+            </div>
+            <!-- /column -->
+            <div class="col-md-4 col-sm-6">
+              <div class="icon"> <i class="budicon-diamond icn"></i> </div>
+              <!-- /.icon -->
+              <div class="text">
+                <h5>Iman</h5>
+                <p>Aenean eu leo quam. Pellente ornare sem lacinia quam venenatis vestibulum sagittis montes, nascetur ridiculus mus.</p>
+              </div>
+              <!-- /.text --> 
+            </div>
+            <!-- /column --> 
+			<div class="divide30"></div>
+            <div class="col-md-4 col-sm-6">
+              <div class="icon"> <i class="budicon-diamond icn"></i> </div>
+              <!-- /.icon -->
+              <div class="text">
+                <h5>pposU</h5>
+                <p>Fusce dapibus commodo, tortor mauris condimentum nibh, ut fermentum massa. Maecenas sed diam eget risus.</p>
+              </div>
+              <!-- /.text --> 
+            </div>
+            <!-- /column -->
+            <div class="col-md-4 col-sm-6">
+              <div class="icon"> <i class="budicon-diamond icn"></i> </div>
+              <!-- /.icon -->
+              <div class="text">
+                <h5>Ijnas</h5>
+                <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id ligula porta felis sit amet non magna sit amet.</p>
+              </div>
+              <!-- /.text --> 
+            </div>
+            <!-- /column -->
+            <div class="col-md-4 col-sm-6">
+              <div class="icon"> <i class="budicon-diamond icn"></i> </div>
+              <!-- /.icon -->
+              <div class="text">
+                <h5>Reppohc</h5>
+                <p>Morbi leo risus, porta ac consectetur ac at eros. Cras mattis consectetur purus. Cras mattis consectetur purus.</p>
+              </div>
+              <!-- /.text --> 
+            </div>
+            <!-- /column --> 
+
+            <div class="col-md-4 col-sm-6">
+              <div class="icon"> <i class="budicon-diamond icn"></i> </div>
+              <!-- /.icon -->
+              <div class="text">
+                <h5>Nibor</h5>
+                <p>Nulla vitae libero, a pharetra augue. Integer posuere a ante venenatis condimentum. Donec id elit non mi.</p>
+              </div>
+              <!-- /.text --> 
+            </div>
+            <!-- /column --> 
+
+            <div class="col-md-4 col-sm-6">
+              <div class="icon"> <i class="budicon-diamond icn"></i> </div>
+              <!-- /.icon -->
+              <div class="text">
+                <h5>Yknarf</h5>
+                <p>Nulla vitae libero, a pharetra augue. Integer posuere a ante venenatis condimentum. Donec id elit non mi.</p>
+              </div>
+              <!-- /.text --> 
+            </div>
+            <!-- /column --> 
+
+            <div class="col-md-4 col-sm-6">
+              <div class="icon"> <i class="budicon-diamond icn"></i> </div>
+              <!-- /.icon -->
+              <div class="text">
+                <h5>Koorb</h5>
+                <p>Nulla vitae libero, a pharetra augue. Integer posuere a ante venenatis condimentum. Donec id elit non mi.</p>
+              </div>
+              <!-- /.text --> 
+            </div>
+            <!-- /column --> 
+
+
+          </div>
+          <!-- /.row --> 
+        </div>
+        <!-- /.services --> 
+        
+      </div>
+      <!-- /.box --> 
+    </section>
+    <!-- /#about -->
+    
+    <section id="contact">
+      <div class="box">
+        <h2 class="section-title">Contact Me</h2>
+        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+        <div class="divide20"></div>
+        <div class="row text-center services-2">
+          <div class="col-md-3 col-sm-6"> <i class="budicon-map"></i>
+            <p>Republic of the Philippines</p>
+          </div>
+          <div class="col-md-3 col-sm-6"> <i class="budicon-telephone"></i>
+            <p>00 (123) 456 78 90 <br />
+              00 (987) 654 32 10 </p>
+          </div>
+          <div class="col-md-3 col-sm-6"> <i class="budicon-mobile"></i>
+            <p>00 (123) 456 78 90 <br />
+              00 (987) 654 32 10 </p>
+          </div>
+          <div class="col-md-3 col-sm-6"> <i class="budicon-mail"></i>
+            <p> <a class="nocolor" href="#">yves@lorenzo.com</a>
+          </div>
+        </div>
+        <!-- /.services-2 -->
+        <div class="divide30"></div>
+        <div class="form-container">
+          <div class="response alert alert-success"></div>
+          <form class="forms" action="contact/form-handler.php" method="post">
+            <fieldset>
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-row text-input-row name-field">
+                    <label>Name</label>
+                    <input type="text" name="name" class="text-input defaultText required"/>
+                  </div>
+                  <div class="form-row text-input-row email-field">
+                    <label>Email</label>
+                    <input type="text" name="email" class="text-input defaultText required email"/>
+                  </div>
+                  <div class="form-row text-input-row subject-field">
+                    <label>Subject</label>
+                    <input type="text" name="subject" class="text-input defaultText"/>
+                  </div>
+                </div>
+                <div class="col-sm-6 lp5">
+                  <div class="form-row text-area-row">
+                    <label>Message</label>
+                    <textarea name="message" class="text-area required"></textarea>
+                  </div>
+                  <div class="form-row hidden-row">
+                    <input type="hidden" name="hidden" value="" />
+                  </div>
+                  <div class="nocomment">
+                    <label for="nocomment">Leave This Field Empty</label>
+                    <input id="nocomment" value="" name="nocomment" />
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="button-row pull-right">
+                    <input type="submit" value="Send Message" name="submit" class="btn btn-submit bm0" />
+                  </div>
+                </div>
+                <div class="col-sm-6 lp5">
+                  <div class="button-row pull-left">
+                    <input type="reset" value="Clear Message" name="reset" class="btn btn-submit bm0" />
+                  </div>
+                </div>
+                <input type="hidden" name="v_error" id="v-error" value="Required" />
+                <input type="hidden" name="v_email" id="v-email" value="Enter a valid email" />
+              </div>
+            </fieldset>
+          </form>
+        </div>
+        <!-- /.form-container --> 
+        
+      </div>
+      <!-- /.box --> 
+    </section>
+    <!-- /#contact -->
+    
+    <footer class="footer box">
+      <p class="pull-left">All rights reserved. © 2017</p>
+      <ul class="social pull-right">
+        <li><a href="#"><i class="icon-s-rss"></i></a></li>
+        <li><a href="#"><i class="icon-s-twitter"></i></a></li>
+        <li><a href="#"><i class="icon-s-facebook"></i></a></li>
+        <li><a href="#"><i class="icon-s-dribbble"></i></a></li>
+        <li><a href="#"><i class="icon-s-pinterest"></i></a></li>
+        <li><a href="#"><i class="icon-s-instagram"></i></a></li>
+        <li><a href="#"><i class="icon-s-vimeo"></i></a></li>
+      </ul>
+      <div class="clearfix"></div>
+    </footer>
+    <!-- /footer --> 
+    
+  </div>
+  <!-- /.container --> 
+</div>
+<!-- /.body-wrapper --> 
+<script src="style/js/jquery.min.js"></script> 
+<script src="style/js/bootstrap.min.js"></script> 
+<script src="style/js/jquery.themepunch.tools.min.js"></script> 
+<script src="style/js/classie.js"></script> 
+<script src="style/js/plugins.js"></script> 
+<script src="style/js/scripts.js"></script>  
+<script>
+	$.backstretch(["style/images/art/bg1.jpg"]);
+</script>
+</body>
+</html>
