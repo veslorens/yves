c9a9bc35458fdc37b9bc35ffeab16b5f203acc1c
Yves Lorenzo <veslorens@gmail.com>
2017-06-09 20:13:22 +0800

    Add Facebook share code.

--- index.php
+++ index.php
@@ -352,6 +352,20 @@
           <li>
             <a href="#"><i class="icon-s-linkedin"></i></a>
           </li>
+          <li>
+            <!-- Load Facebook SDK for JavaScript -->
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9&appId=1668680750065220";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+            <!-- Your share button code -->
+            <div class="fb-share-button" data-href="http://yves-lorenzo.com/" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fyves-lorenzo.com%2F&amp;src=sdkpreparse">Share</a></div>
+          </li>
           <!-- <li>
             <a href="#"><i class="icon-s-twitter"></i></a>
           </li>
