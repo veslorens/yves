23a61e732409c3f288b6a1435f8bbb364894c50c
Yves Lorenzo <veslorens@gmail.com>
2017-12-10 15:05:23 +0800

    Debug: Facebook Web Sharing.

---
+++ share.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- http://localhost:8888/style/images/art/bg-ori.jpg -->
+
+
+    <meta property="og:site_name" content="Scotch">
+    <meta property="og:url" content="https://scotch.io">  
+    <!-- <meta property="og:type" content="website">  -->
+    <!-- /* or this depending content type */  -->
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="Scotch Web Development">
+    <meta property="og:description" content="Scotch is a web development blog discussing all things programming, development, web, and life.">
+    <meta property="og:image" content="https://www.havefunteaching.com/wp-content/uploads/2013/07/letter-y-video-350x350.png">
+    <meta property="fb:app_id" content="1668680750065220">
+
+    <title>Yves</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+    <h1>Hello, world!</h1>
+
+    <a href="#" 
+      onclick="
+      window.open(
+        'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 
+        'facebook-share-dialog', 
+        'width=626,height=436'); 
+        return false;">
+      Share on Facebook
+    </a>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
