2914bd4b538eb6b24af64c81ce8efa158d34828c
Yves Lorenzo <veslorens@gmail.com>
2017-12-12 14:50:12 +0800

    Change HTML5Shiv source.

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="public/themes/lumos/css/custom-styles.css">
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="style/js/html5shiv.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
   </head>
